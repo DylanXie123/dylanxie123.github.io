62a779e2ac7cca44307559550c98caff02229ffa
DylanXie123 <dylanxie123@outlook.com>
2020-09-27 20:24:33 +0800

    add int, diff, etc

--- src/calculator/calculator.tsx
+++ src/calculator/calculator.tsx
@@ -1,13 +1,12 @@
-import nerdamer from 'nerdamer';
 import React, { useState } from 'react';
 import { addStyles, EditableMathField } from 'react-mathquill';
-const AlgebraLatex = require('algebra-latex')
+const nerdamer = require('nerdamer/all');
 
 addStyles()
 
 export default function Calculator() {
 
-  const initLatex = '1+2';
+  const initLatex = '1+x';
   const [latexExp, setResult] = useState(initLatex);
 
   return (
@@ -16,22 +15,65 @@ export default function Calculator() {
         latex={initLatex}
         onChange={
           (mathField) => setResult(mathField.latex())
-          // (mathField) => console.log(mathField.latex())
         }
       />
+      <p></p>
+      <button hidden={true}>Solve</button>
       <p>{'Latex: ' + latexExp}</p>
       <p>{'Result: ' + calc(latexExp)}</p>
+      <p>{'Solve: ' + solve(latexExp)}</p>
+      <p>{'Integer: ' + integrate(latexExp)}</p>
+      <p>{'Defint: ' + defint(latexExp)}</p>
+      <p>{'Diff: ' + diff(latexExp)}</p>
     </div>
   );
 }
 
 function calc(expression: string): string {
   try {
-    const converter = new AlgebraLatex().parseLatex(expression);
-    let e = nerdamer(converter.toMath());
-    console.log(e.text());
+    let e = nerdamer.convertFromLaTeX(expression);
     return e.evaluate().text();
   } catch (error) {
-    return '';
+    return error;
+  }
+}
+
+function solve(expression: string): string {
+  try {
+    let e = nerdamer.convertFromLaTeX(expression);
+    if (e.variables().length == 1) {
+      return e.solveFor('x').toString();
+    } else {
+      return '';
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+function integrate(expression: string): string {
+  try {
+    let e = nerdamer.convertFromLaTeX(expression);
+    return nerdamer.integrate(e.text());
+  } catch (error) {
+    return error;
+  }
+}
+
+function defint(expression: string): string {
+  try {
+    let e = nerdamer.convertFromLaTeX(expression);
+    return nerdamer.defint(e.text(), 0, 1);
+  } catch (error) {
+    return error;
+  }
+}
+
+function diff(expression: string): string {
+  try {
+    let e = nerdamer.convertFromLaTeX(expression);
+    return nerdamer.diff(e.text());
+  } catch (error) {
+    return error;
   }
 }
