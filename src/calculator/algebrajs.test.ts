481d0c6d1427d4b7c0491319cb2e2884616c9a4f
DylanXie123 <dylanxie123@outlook.com>
2020-10-27 22:50:44 +0800

    add test algebrite test

--- src/calculator/algebrajs.test.ts
+++
@@ -1,9 +0,0 @@
-import alg from 'algebra.js';
-const algl = require('algebra-latex');
-
-test('basic', ()=>{
-  const expStr = '1+5';
-  const algObj = new algl().parseLatex(expStr);
-  const a = algObj.toAlgebra(alg)
-  console.log(a.toString())
-})
