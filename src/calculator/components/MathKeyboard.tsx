f0aa90799c5de199c2983d79ed8d8c03ae694818
DylanXie123 <dylanxie123@outlook.com>
2021-02-23 17:35:33 +0800

    reorganize keyboard

--- src/calculator/components/MathKeyboard.tsx
+++ src/calculator/components/MathKeyboard.tsx
@@ -1,41 +1,154 @@
-import { Button } from "@material-ui/core";
+import { Button, createStyles, makeStyles, Theme } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
+import { Backspace, Delete } from "@material-ui/icons";
 import React, { MouseEventHandler, useContext } from "react";
+import MathView from "react-math-view";
 import { ControllerContext } from "../model/controller";
 
-const keys = ['1', '2', '3', '0', '+', '\\times', '\\sin', '\\frac{#@}{#?}','\\arcsin', '\\div'];
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    grid: {
+      position: "fixed",
+      bottom: 0,
+    },
+  }),
+);
 
 export default function MathKeyboard() {
+  const classes = useStyles();
+
+  return (
+    <Grid className={classes.grid} container justify={'center'}>
+      <Grid container item justify={'center'} xs={8} >
+        {ExtraKeyboard().map(
+          (klist) => (
+            <Grid container item justify={'center'} >
+              {klist.map(
+                (k) => (
+                  <Grid item xs={2}>
+                    <MathKey children={k.children} onclick={k.onclick} />
+                  </Grid>
+                )
+              )}
+            </Grid>
+          )
+        )}
+      </Grid>
+      <Grid container item justify={'center'} xs={8}>
+        {BasicKeyboard().map(
+          (klist) => (
+            <Grid container item justify={'center'} >
+              {klist.map(
+                (l) => (
+                  <Grid item xs={2}>
+                    <MathKey children={l.children} onclick={l.onclick} />
+                  </Grid>
+                )
+              )}
+            </Grid>
+          )
+        )}
+      </Grid>
+    </Grid>
+  );
+}
+
+function ExtraKeyboard(): MathKeyProp[][] {
   const controller = useContext(ControllerContext);
-  return (<Grid>
-    {keys.map((s) =>
-      <MathKey
-        onclick={() => controller.add(s)}>
-        {s}
-      </MathKey>)}
-    <MathKey
-      onclick={() => controller.backspace()}>
-      Backspace
-    </MathKey>
-    <MathKey
-      onclick={() => controller.clear()}>
-      Clear
-    </MathKey>
-  </Grid>);
+
+  const row1: Array<MathKeyProp> = ['\\sin', '\\cos', '\\tan', '\\log'].map((v) => ({
+    children: v,
+    onclick: () => controller.add(v + '(#?)')
+  }));
+
+  row1.push({
+    children: '\\sqrt{#?}',
+    onclick: () => controller.add('\\sqrt{#?}')
+  })
+
+  row1.push({
+    children: '\\sin^{-1}',
+    onclick: () => controller.add('\\arcsin(')
+  })
+
+  const row2: Array<MathKeyProp> = ['(', ')', 'e^{#?}', '{#?}^2'].map((v) => ({
+    children: v,
+    onclick: () => controller.add(v)
+  }));
+
+  return [row1, row2];
+}
+
+function BasicKeyboard(): MathKeyProp[][] {
+  const controller = useContext(ControllerContext);
+
+  const row1: Array<MathKeyProp> = ['7', '8', '9'].map((v) => ({
+    children: v,
+    onclick: () => controller.add(v)
+  }));
+
+  row1.push({
+    children: <Delete />,
+    onclick: () => controller.clear()
+  })
+
+  row1.push({
+    children: <Backspace />,
+    onclick: () => controller.backspace()
+  })
+
+  const row2: Array<MathKeyProp> = ['4', '5', '6', '+', '-'].map((v) => ({
+    children: v,
+    onclick: () => controller.add(v)
+  }));
+
+  const row3: Array<MathKeyProp> = ['1', '2', '3'].map((v) => ({
+    children: v,
+    onclick: () => controller.add(v)
+  }));
+
+  row3.push({
+    children: '\\times',
+    onclick: () => controller.add('*')
+  })
+
+  row3.push({
+    children: '\\div',
+    onclick: () => controller.add('/')
+  })
+
+  const row4: Array<MathKeyProp> = ['0', '.', '\\pi', 'e'].map((v) => ({
+    children: v,
+    onclick: () => controller.add(v)
+  }));
+
+  row4.push({
+    children: '=',
+    onclick: () => controller.add('/')
+  })
+
+  return [row1, row2, row3, row4];
 }
 
 interface MathKeyProp {
   onclick: MouseEventHandler
-  children?: string
+  children?: string | JSX.Element
 }
 
 function MathKey(prop: MathKeyProp) {
   return (
     <Button
-      variant="contained"
+      variant='outlined'
       color="primary"
-      onClick={prop.onclick}>
-      {prop.children}
+      onClick={prop.onclick}
+    >
+      {typeof prop.children === 'string' ?
+        <MathView
+          value={prop.children}
+          readOnly={true}
+          style={{ outline: 0 }}
+        /> :
+        prop.children}
     </Button>
   );
 }
