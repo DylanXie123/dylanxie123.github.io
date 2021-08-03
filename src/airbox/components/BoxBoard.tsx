eaf617d53590f45cf97cc60645e736e8d6d23d92
DylanXie123 <dylanxie123@outlook.com>
2021-08-03 20:55:48 +0800

    fix(airbox): fix text overflow in each box
    remove material library and use native dom

--- src/airbox/components/BoxBoard.tsx
+++ src/airbox/components/BoxBoard.tsx
@@ -3,35 +3,22 @@ import { AirBoxModelContext } from '../model/box_model';
 import { observer } from 'mobx-react-lite';
 import BoxCard from './BoxCard';
 import AddBoxCard from './AddBoxCard';
-import GridList from '@material-ui/core/GridList/GridList';
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import createStyles from '@material-ui/core/styles/createStyles';
-import { GridListTile } from '@material-ui/core';
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    grid: {
-      padding: 10,
-    },
-  }),
-);
 
 const BoxBoard = observer(() => {
   const airBoxModel = useContext(AirBoxModelContext);
-  const classes = useStyles();
 
   const grids = airBoxModel.boxes.map(box => (
-    <GridListTile key={box.id}>
+    <li key={box.id}>
       <BoxCard box={box} />
-    </GridListTile>
+    </li>
   ));
 
   grids.push(<AddBoxCard key='add'/>);
 
   return (
-    <GridList className={classes.grid} cols={5}>
+    <ul style={{padding: 10}}>
       {grids}
-    </GridList>
+    </ul>
   );
 })
 
