f2e7616c939c84e7d46f60a0cd2b0d73ffad8dec
DylanXie123 <dylanxie123@outlook.com>
2021-08-15 21:09:15 +0800

    feat(airbox): make url content clickable

--- src/airbox/components/BoxCard.tsx
+++ src/airbox/components/BoxCard.tsx
@@ -8,13 +8,53 @@ interface BoxCardProp {
 
 const BoxCard = (prop: BoxCardProp) => {
   const airBoxModel = useContext(AirBoxModelContext);
-
   return (
-    <div style={{border: '1px solid'}}>
-      {prop.box.content}
-      <button onClick={()=>{airBoxModel.delete(prop.box.id)}}>Delete</button>
+    <div style={{ border: '1px solid' }}>
+      {UrlBox(prop.box.content)}
+      <button onClick={() => { airBoxModel.delete(prop.box.id) }}>Delete</button>
     </div>
   );
+}
+
+const UrlBox = (content: string) => {
+  const urlRegexp = /https?:\/\/[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_.~#?&//=]*/g;
+  const urls = Array.from(content.matchAll(urlRegexp));
+
+  if (urls.length === 0) {
+    return <span>{content}</span>
+  }
+
+  const elements: Array<JSX.Element> = [];
+  for (let index = 0; index < urls.length; index++) {
+    // text-url-text-...-url-text
+    const url = urls[index];
+    const link = content.slice(url.index, url.index! + url[0].length);
+
+    if (index === 0 && url.index! > 0) {
+      elements.push(
+        <span key={'textSpan-begin'}>{content.slice(0, url.index)}</span>
+      );
+    }
+
+    if (index === urls.length - 1) {
+      const textContent = content.slice(url.index! + link.length, content.length);
+      elements.push(
+        <a key={`a-${index}`} href={link} target={'_blank'} rel={'noreferrer'}>{link}</a>,
+        <span key={`textSpan-${index}`}>{textContent}</span>
+      );
+      break;
+    }
+
+    // later str
+    const laterUrl = urls[index + 1];
+    const textContent = content.slice(url.index! + link.length, laterUrl.index);
+    elements.push(
+      <a key={`a-${index}`} href={link} target={'_blank'} rel={'noreferrer'}>{link}</a>,
+      <span key={`textSpan-${index}`}>{textContent}</span>
+    );
+    
+  }
+  return elements
 }
 
 export default BoxCard
