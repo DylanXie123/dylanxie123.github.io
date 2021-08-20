ddf639b026ee3067f3fe7b9d04ff1fb8a1344242
DylanXie123 <dylanxie123@outlook.com>
2021-08-20 19:05:23 +0800

    refactor(movie): remove material ui library

--- src/movie/components/MovieCard.tsx
+++ src/movie/components/MovieCard.tsx
@@ -1,44 +1,19 @@
-import Card from "@material-ui/core/Card/Card";
-import CardContent from "@material-ui/core/CardContent/CardContent";
-import CardMedia from "@material-ui/core/CardMedia/CardMedia";
-import createStyles from "@material-ui/core/styles/createStyles";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 import React from "react";
 import { Movie } from "../model/movie_model";
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    card: {
-      width: 160,
-      height: 200,
-    },
-    media: {
-      height: 120,
-    },
-    content: {
-      height: 80,
-    }
-  }),
-);
 
 interface MovieCardProp {
   movie: Movie
 }
 
 const MovieCard = (prop: MovieCardProp) => {
-  const classes = useStyles();
-
   return (
-    <Card className={classes.card} raised={true}>
-      <CardMedia
-        className={classes.media}
-        title={prop.movie.title}
-        image={prop.movie.poster}
+    <div style={{ border: '1px solid' }}>
+      <img
+        src={prop.movie.poster}
+        alt={prop.movie.title}
       />
-      <CardContent className={classes.content}>
-        {prop.movie.title}
-      </CardContent>
-    </Card>
+      <p> {prop.movie.title} </p>
+    </div>
   );
 }
 
