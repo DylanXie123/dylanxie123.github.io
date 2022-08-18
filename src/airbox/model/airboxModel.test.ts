2dc44f434fe4d027f13177f35ea7241ed190e22f
DylanXie123 <dylanxie123@outlook.com>
2022-08-18 21:59:07 +0800

    test(airbox): add test for supabase failure
    use domock to mock supabase in different way

--- src/airbox/model/airboxModel.test.ts
+++ src/airbox/model/airboxModel.test.ts
@@ -1,5 +1,6 @@
+import { PostgrestResponse } from "@supabase/supabase-js";
 import AirBoxModel from "./airboxModels"
-import ContentModel from "./contentModel";
+import ContentModel, { createContentModel } from "./contentModel";
 
 jest.mock("../../login/auth", () => {
   return {
@@ -7,34 +8,111 @@ jest.mock("../../login/auth", () => {
   }
 })
 
-jest.mock("@supabase/supabase-js", () => {
-  const createMockValue = (item: ContentModel) =>
-    Promise.resolve({
-      data: [item]
-    });
+type ResponseFormat = Promise<Partial<PostgrestResponse<ContentModel>>>;
 
-  const supabase = {
-    from: () => ({
-      insert: (item: ContentModel) => createMockValue(item)
-    })
-  }
+interface MockSupbaseConfig {
+  insertResponse?: (item: ContentModel) => ResponseFormat,
+  deleteResponse?: () => ResponseFormat,
+  selectResponse?: () => ResponseFormat,
+}
 
-  return {
-    createClient: jest.fn().mockReturnValue(supabase)
-  }
-})
+const mockSuccessResponse = (items: ContentModel[]) =>
+  Promise.resolve({
+    data: items,
+    error: null,
+  });
+
+const mockFailureResponse = () =>
+  Promise.resolve({
+    data: null,
+    error: {
+      message: "this is an error message",
+      hint: "",
+      details: "",
+      code: ""
+    }
+  });
+
+const fakeValue = createContentModel("fake content", "tetx/plain");
+
+const defaultConfig: MockSupbaseConfig = {
+  insertResponse: (item: ContentModel) => mockSuccessResponse([item]),
+  deleteResponse: () => mockSuccessResponse([fakeValue]),
+  selectResponse: () => mockSuccessResponse([fakeValue]),
+}
+
+const configureSupabase = (config = defaultConfig) => {
+  jest.doMock("@supabase/supabase-js", () => {
+
+    const supabase = {
+      from: () => ({
+        insert: config.insertResponse,
+        delete: () => ({
+          match: config.deleteResponse,
+        }),
+        select: () => ({
+          order: config.selectResponse,
+        })
+      })
+    }
+
+    return {
+      __esModule: true,
+      createClient: () => supabase,
+    }
+  })
+}
 
 describe("airbox model test", () => {
-  let airboxModel = new AirBoxModel();
+  let MockAirBoxModel: typeof AirBoxModel;
+
+  beforeAll(async () => {
+    configureSupabase();
+    const AirBoxModel = await import("./airboxModels");
+    MockAirBoxModel = AirBoxModel.default;
+  });
+
+  afterAll(() => jest.resetModules());
 
   it("should be empty", () => {
+    let airboxModel = new MockAirBoxModel();
     expect(airboxModel.getModels.length).toBe(0);
   });
 
   it("should create text", async () => {
+    let airboxModel = new MockAirBoxModel();
     const result = await airboxModel.createText("hi");
     expect(result).toBe(true);
     expect(airboxModel.getModels.length).toBe(1);
     expect(airboxModel.getModels[0].content).toBe("hi");
   })
+})
+
+/// supabase will return network failure in error
+/// https://github.com/supabase/supabase-js/issues/32#issuecomment-1061383837
+describe("supabase failure airbox model test", () => {
+  let MockAirBoxModel: typeof AirBoxModel;
+
+  beforeAll(async () => {
+    configureSupabase({
+      insertResponse: () => mockFailureResponse()
+    })
+    const AirBoxModel = await import("./airboxModels");
+    MockAirBoxModel = AirBoxModel.default;
+  });
+
+  afterAll(() => jest.resetModules());
+
+  it("should not create text", async () => {
+    let airboxModel = new MockAirBoxModel();
+    const result = airboxModel.createText("hi");
+    expect(airboxModel.getModels.length).toBe(1);
+    expect(airboxModel.getUpdatingItemID).not.toBe(null);
+    result.then(v => {
+      expect(v).toBe(false);
+      expect(airboxModel.getModels.length).toBe(0);
+      expect(airboxModel.getUpdatingItemID).toBe(null);
+    })
+  })
+
 })
